e0c2196e250431f639e57d5b87898e3d8f9b6307
Kate Lu <kevinlu98@gmail.com>
2025-12-15 21:11:50 -0800

    updated dependencies, added chat test

--- main.go
+++ main.go
@@ -10,22 +10,21 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-//Variables for parameters
+// Variables for parameters
 var (
 	BotToken string
 )
 
-//Define the bot token
+// Define the bot token
 func init() {
 	flag.StringVar(&BotToken, "t", "", "Bot Token")
 	flag.Parse()
 }
 
-//
 func main() {
-	
+	//Create bot session
 	dg, err := discordgo.New("Bot " + BotToken)
-	
+
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
@@ -33,10 +32,33 @@ func main() {
 
 	dg.AddHandler(messageCreate)
 
+	//Declare intents
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
+	//Open session
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
+	}
+
+	//Wait for termination
+	fmt.Println("Yuni Bot connected to Discord.")
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sigchan
+
+	dg.Close()
+}
+
+// Check every message if it contains trigger words and respond
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	//Bot ignores messages created by itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "test" {
+		s.ChannelMessageSend(m.ChannelID, "reply")
 	}
 }
