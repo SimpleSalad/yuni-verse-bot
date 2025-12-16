9d08e95ed7dbd02b29d09aeee905bee224a9de7d
SimpleSalad <SimpleSalad@users.noreply.github.com>
2025-12-15 19:56:03 -0800

    add go.mod

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/SimpleSalad/yuni-verse-bot
+
+go 1.25
+
+require (
+	github.com/bwmarrin/discordgo v0.29.0
+)
--- main.go
+++ main.go
@@ -10,17 +10,33 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+//Variables for parameters
 var (
 	BotToken string
 )
 
+//Define the bot token
 func init() {
 	flag.StringVar(&BotToken, "t", "", "Bot Token")
 	flag.Parse()
 }
 
+//
 func main() {
 	
 	dg, err := discordgo.New("Bot " + BotToken)
 	
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+	}
 }
